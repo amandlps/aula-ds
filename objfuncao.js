3da667ee16b34f03f7e4d39ae326bbee0dfd304a
amanda.lopes.2008 <amanda.lopes.2008@aluno.edu.pi.gov.br>
2026-06-17 14:14:42 +0000

    funcao

--- objfuncao.js
+++ objfuncao.js
@@ -2,8 +2,8 @@ const calculadora={
     numero:10,
     numero2:5,
     multiplicar(){
-        return this.numero1*this.numero2}
-    dividir
+        return this.numero1*this.numero2},
+    dividir(){
      return this.numero1/this.numero2},
     somar(){
         return this.numero+this.numero2},
